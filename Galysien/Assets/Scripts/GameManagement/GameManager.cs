6948a1e9b2f1ab3061b7d6313db9fd8856cb3f81
dwoojinkim <dwoojinkim@gmail.com>
2020-02-26 18:16:30 -0500

    Going back to broken version
    Going back to try and fix it. Also making a new branch.

--- Galysien/Assets/Scripts/GameManagement/GameManager.cs
+++ Galysien/Assets/Scripts/GameManagement/GameManager.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public enum TileType { None, Wall, Trap };
 public enum Element { None, Earth, Fire, Water, Dark, Light };
 public enum Sigil { None, Up, Down, Left, Right };             //Temporary Sigils
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject Player1Object;
     public GameObject card;
 
-    private PlayerManager player1;
     private Camera mainCamera;
     private GameGrid grid;
     private int initialCardDrawAmount = 4;
@@ -40,7 +37,6 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         mainCamera = Camera.main;
-        player1 = Player1Object.GetComponent<PlayerManager>();
 
         grid = FindObjectOfType<GameGrid>();
         hoverCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -113,8 +109,6 @@ public class GameManager : MonoBehaviour
                 if (cardPool[i].activeSelf)
                     cardPool[i].transform.localPosition = new Vector3(cardPool[i].transform.localPosition.x, -3.5f, cardPool[i].transform.localPosition.z);
             }
-
-            DeactivateHoverCube();
         }
     }
 
@@ -122,8 +116,6 @@ public class GameManager : MonoBehaviour
     {
         currentNumHand = initialCardDrawAmount;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         for (int i = 0; i < currentNumHand; i++)
             DrawCard();
 
@@ -137,10 +129,6 @@ public class GameManager : MonoBehaviour
 
     private void SetDrawnCardPositions()
     {
-=======
->>>>>>> parent of b173668... Card Selection/Tile Placement Fix
-=======
->>>>>>> parent of b173668... Card Selection/Tile Placement Fix
         int halfCards;
         float cardPosX;
         GameObject card;
@@ -185,8 +173,7 @@ public class GameManager : MonoBehaviour
                                                                                  selectedCard.GetComponent<Renderer>().material.color.g,
                                                                                  selectedCard.GetComponent<Renderer>().material.color.b,
                                                                                  0.5f);
-                //Ignore Raycast layer
-                selectedCard.layer = 2;         
+                selectedCard.layer = 2;         //Ignore Raycast layer
 
                 //Set so that if the camera moves around a bit, the card won't move with it
                 initialCameraPosition = mainCamera.transform.position;
@@ -247,23 +234,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     private void DeactivateHoverCube()
     {
-            hoverCube.GetComponent<Renderer>().material.SetFloat("_Mode", 2);   //2 = fade mode
+        hoverCube.GetComponent<Renderer>().material.SetFloat("_Mode", 2);   //2 = fade mode
 
-            //TODO: More efficient way to color instead of constantly making a new color when hovered over grid.
-            hoverCube.GetComponent<Renderer>().material.color = new Color(0.4f, 0.4f, 1.0f, 0.0f);
+        //TODO: More efficient way to color instead of constantly making a new color when hovered over grid.
+        hoverCube.GetComponent<Renderer>().material.color = new Color(0.4f, 0.4f, 1.0f, 0.0f);
 
-            hoverCube.transform.position = hoverPoint;
+        hoverCube.transform.position = hoverPoint;
     }
 
     //Moves the selected card to the position of the mouse.
-=======
->>>>>>> parent of b173668... Card Selection/Tile Placement Fix
-=======
->>>>>>> parent of b173668... Card Selection/Tile Placement Fix
     private void MoveSelectedCard(GameObject card)
     {
         toObjectVector = card.transform.position - initialCameraPosition;
